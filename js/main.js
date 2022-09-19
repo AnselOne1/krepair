5b07437e0da2f8aa8c64255773845dcade426aa5
AnselOne1 <oneansel@gmail.com>
2022-09-19 13:48:45 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$(function () {
+
+
+    $('.slider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        dots: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="img/brand/arrow-left.svg" alt="предыдущий слайд"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="img/brand/arrow-right.svg" alt="следующий слайд"></button>'
+    });
+
+
+
+
+
+});
